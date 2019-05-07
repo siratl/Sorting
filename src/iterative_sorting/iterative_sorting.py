a86dfb6961f0467572d3129ed7322985d55e3f7c
siratl <elishubax@gmail.com>
2019-05-07 15:57:23 -0700

    Implemented the bubble and merge sort method

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -23,6 +23,17 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    swapped = True
+    while swapped:
+        swapped = False 
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+
+                # swap
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = True
 
     return arr
 
